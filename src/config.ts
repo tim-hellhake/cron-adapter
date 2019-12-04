c4fea06974cbfb10717c29549ebdf1330a600abf
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2019-12-04 16:27:34 +0000

    Add groups (#2)

---
+++ src/config.ts
@@ -0,0 +1,97 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+ */
+
+import { Timezone } from "tz-offset";
+import { Database } from "gateway-addon";
+import crypto from 'crypto';
+
+export interface Config {
+    timezone: Timezone,
+    cronJobs: CronJobV1[]
+}
+
+export interface CronJobV1 {
+    id: string,
+    name: string,
+    cron: string
+}
+
+export interface ConfigV2 {
+    timezone: Timezone,
+    cronGroups: CronGroup[]
+}
+
+export interface CronGroup {
+    id: string,
+    name: string,
+    cronJobs: CronJobV2[]
+}
+
+export interface CronJobV2 {
+    name: string,
+    cron: string
+}
+
+export async function load(manifest: any): Promise<ConfigV2> {
+    const database = new Database(manifest.name);
+    await database.open();
+    const config: Config & ConfigV2 = await database.loadConfig();
+    const configv2 = asConfigV2(config);
+    await database.saveConfig(configv2);
+
+    return configv2;
+}
+
+function asConfigV2(config: Config & ConfigV2): ConfigV2 {
+    const {
+        timezone,
+        cronJobs,
+        cronGroups
+    } = config;
+
+    return {
+        timezone,
+        cronGroups: [...asCronGroups(cronJobs || []), ...(cronGroups || []).map(withId)]
+    };
+}
+
+function asCronGroups(cronJobs: CronJobV1[]): CronGroup[] {
+    return cronJobs.map(cronJob => {
+        const {
+            id,
+            name,
+            cron
+        } = cronJob;
+
+        console.log(`Migrating ${name} to v2`);
+
+        return {
+            id,
+            name,
+            cronJobs: [{ name: 'fired', cron }]
+        };
+    });
+}
+
+function withId<T extends { id: string, name: string }>(obj: T): T {
+    const {
+        id,
+        name
+    } = obj
+
+    if (!id) {
+        const generatedId = crypto.randomBytes(16).toString("hex");
+
+        console.log(`Generated id ${generatedId} for ${name}`);
+
+        return {
+            ...obj,
+            id: generatedId
+        }
+    }
+
+    return obj;
+}
--- src/cron-adapter.ts
+++ src/cron-adapter.ts
@@ -4,75 +4,63 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
  */
 
-import { Adapter, Device, Event, Database } from 'gateway-addon';
-
-import crypto from 'crypto';
-
+import { Adapter, Device, Event } from 'gateway-addon';
 import { schedule } from 'node-cron';
 import { Timezone } from 'tz-offset';
+import { load, CronJobV2 } from './config';
 
 const CRON_EVENT = 'fired';
 
 class CronDevice extends Device {
-    constructor(adapter: any, id: string, name: string, cron: string, timezone: Timezone) {
+    constructor(adapter: any, id: string, name: string, cronJobs: CronJobV2[], timezone: Timezone) {
         super(adapter, id);
         this['@context'] = 'https://iot.mozilla.org/schemas/';
         this.name = name;
 
-        this.events.set(CRON_EVENT, {
-            name: CRON_EVENT,
-            metadata: {
-                description: 'Cron job fired',
-                type: 'string'
-            }
-        });
+        for(const cronJob of cronJobs) {
+            const {
+                name,
+                cron
+            } = cronJob;
 
-        schedule(cron, () => {
-            this.eventNotify(new Event(this, CRON_EVENT));
-        }, { timezone });
+            this.events.set(name, {
+                name: name,
+                metadata: {
+                    description: `Cron job ${name} fired`,
+                    type: 'string'
+                }
+            });
+    
+            schedule(cron, () => {
+                this.eventNotify(new Event(this, CRON_EVENT));
+            }, { timezone });
+        }
     }
 }
 
 export class CronAdapter extends Adapter {
-    private readonly database: Database;
-
     constructor(addonManager: any, manifest: any) {
         super(addonManager, CronAdapter.name, manifest.name);
-        this.database = new Database(manifest.name)
         addonManager.addAdapter(this);
-        this.createCronJobs();
+        this.createCronJobs(manifest);
     }
 
-    private async createCronJobs() {
+    private async createCronJobs(manifest: any) {
         const {
             timezone,
-            cronJobs
-        } = await this.loadConfig();
+            cronGroups
+        } = await load(manifest);
 
-        if (cronJobs) {
-            for (const cronJob of cronJobs) {
-                const cron = new CronDevice(this, cronJob.id, cronJob.name, cronJob.cron, timezone);
+        if (cronGroups) {
+            for (const cronGroup of cronGroups) {
+                const {
+                    id,
+                    name,
+                    cronJobs
+                } = cronGroup;
+                const cron = new CronDevice(this, id, name, cronJobs, timezone);
                 this.handleDeviceAdded(cron);
             }
         }
-    }
-
-    private async loadConfig() {
-        await this.database.open();
-        const config = await this.database.loadConfig();
-        const {
-            cronJobs
-        } = config;
-
-        if (cronJobs) {
-            for (const cronJob of cronJobs) {
-                if (!cronJob.id) {
-                    cronJob.id = crypto.randomBytes(16).toString("hex");
-                }
-            }
-        }
-
-        await this.database.saveConfig(config);
-        return config;
     }
 }
